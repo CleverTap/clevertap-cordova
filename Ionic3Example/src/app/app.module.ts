0af686d387fbf116eaca268cfb4e311bb883f661
Peter Wilkniss <peter@clevertap.com>
2018-07-29 11:46:52 -0700

    update ionic3 example

--- Ionic3Example/src/app/app.module.ts
+++ Ionic3Example/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CleverTap } from '@ionic-native/clevertap';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    CleverTap,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
