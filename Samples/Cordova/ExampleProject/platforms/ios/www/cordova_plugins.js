cb1a3fe9d774eccfe522b3a53b5a967176fefc77
ansh <ansh@clevertap.com>
2022-05-25 21:53:14 +0530

    [SDK-1605] updating sample app part 7

---
+++ Samples/Cordova/ExampleProject/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "clevertap-cordova.CleverTap",
+      "file": "plugins/clevertap-cordova/www/CleverTap.js",
+      "pluginId": "clevertap-cordova",
+      "clobbers": [
+        "CleverTap"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "clevertap-cordova": "2.5.0"
+  };
+});
